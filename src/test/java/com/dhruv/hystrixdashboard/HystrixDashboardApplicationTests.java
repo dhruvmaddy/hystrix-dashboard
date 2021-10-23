20b945a6d77d2e987a2d8ea2b03f6c76d30ce7fa
Dhruv Prakash <40271648+dhruvmaddy@users.noreply.github.com>
2021-10-23 19:50:03 +0530

    Initial commit

---
+++ src/test/java/com/dhruv/hystrixdashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhruv.hystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
